c57e42d22fdad5ad6a3a66e0949d0ec2e59d4290
moLinee <molinee007@gmail.com>
2023-11-10 22:20:46 +0800

    本地commit

---
+++ src/main/java/mavendemo/MavenDemo.java
@@ -0,0 +1,19 @@
+package mavendemo;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * author:Linee
+ * date:2023/11/6 21:39
+ * description:
+ */
+public class MavenDemo {
+
+    public static void main(String[] args) {
+        if (StrUtil.isNotBlank("")) {
+            System.out.println(1);
+        }else {
+            System.out.println(2);
+        }
+    }
+}
